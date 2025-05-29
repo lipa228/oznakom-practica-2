d243847121da864531f30ca2e2fc1ac2523ac6d5
lipa228 <kozlovir006@mail.ru>
2025-05-29 18:49:01 +0300

    Add files via upload

--- client.c
+++ client.c
@@ -12,15 +12,15 @@
 int main(int argc, char* argv[]) {
     // filling address struct
     struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(PORT);
+	server_address.sin_addr.s_addr = INADDR_ANY;
 
     // socket desriptor
     int network_socket;
 
     // connecting socket
-    network_socket = socket(AF_INET, SOCK_STREAM, 0);
+	network_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (network_socket < 0) {
         perror("Failed to connect socket...");
         exit(EXIT_FAILURE);
@@ -31,14 +31,14 @@ int main(int argc, char* argv[]) {
         perror("Failed to connect to server...");
         close(network_socket);
         exit(EXIT_FAILURE);
-    }
+    }    
 
     // sending information
-    char message[256] = "Sukhorukov Alexander Vladislavovich KKSO-26-24 1 kurs";
+    char message[256] = "Kozlov Ilya Ruslanovich  KKSO-26-24 1 kurs";
     send(network_socket, message, strlen(message), 0);
 
-    // then close the socket
-    close(network_socket);
+	// then close the socket
+	close(network_socket);
 
     return EXIT_SUCCESS;
 }
